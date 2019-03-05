36ce126a5b35dc2374b790f98142293edd044af8
fanwenhan <fanwenhan@quarkioe.com>
2019-03-05 16:31:31 +0800

    增加轮廓提取

--- src/contours.py
+++ src/contours.py
@@ -4,16 +4,9 @@ import numpy as np
 import argparse
 import cv2
 
-
-def test():
-    filename = "E:\比赛\tianchi\tianchi_Jinnan_Digital_Manufacturing\datas\test_pictures\category_id_1_feature_gray\190109_182654_00154240.jpg".replace(
-        '\\', "\\\\")
-    print(filename)
-    img = cv2.imread(filename)
-    print(img)
-    quit()
-
-
+'''
+    Canny算子计算边缘轮廓
+'''
 def edge(img):
     # 高斯滤波,降低噪声
     blur = cv2.GaussianBlur(img, (3, 3), 0)
@@ -30,21 +23,16 @@ def edge(img):
     
     return edge_out
 
-
 '''
     检测目标区域
     @param img: 图像帧
     @param threshold: 二值化的像素上下限
 '''
-
-
-def targetDetect(img, threshold=[]):
+def targetDetect(img, threshold=(0, 255)):
     if img is not None:  # 判断图片是否读入
         low, high = threshold
         
-        kernel_2 = np.ones((2, 2), np.uint8)  # 2*2卷积核
-        kernel_3 = np.ones((3, 3), np.uint8)  # 3*3卷积核
-        kernel_4 = np.ones((4, 4), np.uint8)  # 4*4卷积核
+        kernel = np.ones((3, 3), np.uint8)  # 3*3卷积核
         
         HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # 把BGR图像转换为HSV格式
         
@@ -58,14 +46,14 @@ def targetDetect(img, threshold=[]):
         mask = cv2.inRange(img, lower, upper)
         
         '''
-            下面四行是用卷积进行滤波：
-            erosion： 侵蚀
-            dilation： 扩张
+            下面四行是用卷积进行滤波，形态学算法：
+            erosion： 腐蚀
+            dilation： 膨胀
         '''
-        erosion = cv2.erode(mask, kernel_3, iterations=1)
-        erosion = cv2.erode(erosion, kernel_3, iterations=1)
-        dilation = cv2.dilate(erosion, kernel_3, iterations=1)
-        dilation = cv2.dilate(dilation, kernel_3, iterations=1)
+        erosion = cv2.erode(mask, kernel, iterations=1)
+        erosion = cv2.erode(erosion, kernel, iterations=1)
+        dilation = cv2.dilate(erosion, kernel, iterations=1)
+        dilation = cv2.dilate(dilation, kernel, iterations=1)
         
         # target是把原图中的非目标颜色区域去掉剩下的图像
         target = cv2.bitwise_and(img, img, mask=dilation)
@@ -78,6 +66,7 @@ def targetDetect(img, threshold=[]):
         binary, contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         # contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
+        # 按照轮廓从大到小排序
         c = sorted(contours, key=cv2.contourArea, reverse=True)[0]
         x, y, w, h = cv2.boundingRect(c)  # 将轮廓分解为识别对象的左上角坐标和宽、高
         # 在图像上画上矩形（图片、左上角坐标、右下角坐标、颜色、线条宽度）
@@ -93,11 +82,10 @@ def targetDetect(img, threshold=[]):
 
 
 if __name__ == '__main__':
-    # pic_name = "../datas/category_id_1/190118_202347_00164778.jpg"
-    pic_name = "/home/wenhan/study/2019天池竞赛/津南数字制造算法挑战赛/jinnan2_round1_test_a_20190222/190109_183554_00154272.jpg"
+    pic_name = "../../jinnan2_round1_test_a_20190222/190109_183554_00154272.jpg"
     img = cv2.imread(pic_name)  # 读取图片
     
-    targetDetect(img, [0, 255])
+    targetDetect(img, (0, 255))
 
 
 
